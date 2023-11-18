4a42f70d69dadc9c521f9993e36df056afec900b
dlee371 <61858877+dlee371@users.noreply.github.com>
2023-11-18 17:03:09 -0600

    made some updates regarding member's names

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const txt1 = document.getElementById("f_name")
+        const btn1 = document.getElementById("btn1")
+        const out1 = document.getElementById('output1')
+
+        function fun1() {
+            out1.innerHTML = txt1.value;
+        }
+
+        btn1.addEventListener('click', fun1)
+
+function user(first, last, number){
+
+}
