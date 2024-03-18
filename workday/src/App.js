542780ae0e1a605da50e812cf73009cd1213983c
dlee371 <61858877+dlee371@users.noreply.github.com>
2024-03-18 15:56:59 -0500

    figuring out backend for adding jobs

--- workday/src/App.js
+++ workday/src/App.js
@@ -1,9 +1,39 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client';
 import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
+
+  const [setAddJob, setShowAddJob] = useState (false)
+
+  const [jobs, setJobs] = useState([
+    {
+      id: 1,
+      text: "Moving furniture",
+      day: "4/20 at 4:20",
+      loc: "1234 Asdf Blvd.",
+      peeps: 4,
+      signUpButton: <button>Sign up</button>
+    },
+    {
+      id: 2,
+      text: "Raking leaves",
+      day: "4/20 at 4:20",
+      loc: "42069 Asdf Blvd.",
+      peeps: 4,
+      signUpButton: <button>Sign up</button>
+    }
+  ])
+
+  //Add Job
+  const addJob = (job) => {
+    // console.log(job);
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newJob = { id, ...job}
+    setJobs([...jobs, newJob])
+  }
+
+  //delete job
+
 
   return (
     <div>
