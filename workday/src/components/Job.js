944db6602c7ac4dee266df9e44c1a021f1567457
dlee371 <61858877+dlee371@users.noreply.github.com>
2024-03-21 14:55:23 -0500

    added delete button, need to revise display of day and time

--- workday/src/components/Job.js
+++ workday/src/components/Job.js
@@ -1,10 +1,13 @@
-// import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
+// import { FaCalendar } from 'react-icons/fa'
+
 
 const Job = ({ job, onDelete }) => {
     return (
         <div className='box' >
-            <div className='gift'>
-            <p>{job.text}</p>
+            <div className='gift' >
+            <h3>{job.text}{' '} <FaTimes style={{color:"red", cursor:'pointer'}} 
+            onClick={() => onDelete(job.id)}/></h3>
             <p>{job.day}</p>
             <p>{job.loc}</p>
             <p>Number of bros needed: {job.peeps}</p>
