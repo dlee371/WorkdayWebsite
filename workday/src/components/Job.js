c7326ad13ef26d8cbb748a0368c885198fee222b
dlee371 <61858877+dlee371@users.noreply.github.com>
2024-03-21 21:27:25 -0500

    finalized date time formatting

--- workday/src/components/Job.js
+++ workday/src/components/Job.js
@@ -1,19 +1,20 @@
-import { FaTimes } from 'react-icons/fa'
-
+import { FaTimes } from 'react-icons/fa';
+import { format } from 'date-fns';
 
 
 
 const Job = ({ job, onDelete }) => {
-    const date = new Date(job.day);
+    const dateTime = new Date(job.day);
+    const f = format(dateTime, "dd MMM yyyy hh:mm a")
     
-    const f = new Intl.DateTimeFormat("en-us", {dateStyle:'full'}, );
+    // const f = new Intl.DateTimeFormat("en-us", {dateStyle:'full'}, );
     return (
         <div className='box' >
             <div className='gift' >
                 
             <h3>{job.text}{' '} <FaTimes style={{color:"red", cursor:'pointer'}} 
             onClick={() => onDelete(job.id)}/></h3>
-            <p>{f.format(date)}</p>
+            <p>{f}</p>
             <p>{job.loc}</p>
             <p># of bros needed: {job.peeps}</p>
             </div>
