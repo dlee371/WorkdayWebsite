4691f3d8f5b94c638ba673d3e838d1518f2e2650
dlee371 <61858877+dlee371@users.noreply.github.com>
2024-03-25 10:52:37 -0500

    adjusted some styling stuff, making progress on trying to make a "table"

--- workday/src/App.js
+++ workday/src/App.js
@@ -7,26 +7,10 @@ function App() {
 
   const [showAddJob, setShowAddJob] = useState (false)
 
-  const [jobs, setJobs] = useState([
-    // {
-    //   id: 1,
-    //   text: "Moving furniture",
-    //   day: "4/20 at 4:20",
-    //   loc: "1234 Asdf Blvd.",
-    //   peeps: 4,
-    // },
-    // {
-    //   id: 2,
-    //   text: "Raking leaves",
-    //   day: "4/20 at 4:20",
-    //   loc: "42069 Asdf Blvd.",
-    //   peeps: 4,
-    // }
-  ])
+  const [jobs, setJobs] = useState([])
 
   //Add Job
   const addJob = (job) => {
-    console.log(job)
     // console.log(job);
     const id = Math.floor(Math.random() * 10000) + 1
     const newJob = { id, ...job}
@@ -39,13 +23,17 @@ function App() {
   }
 
   return (
-    <div className='container' >
+    <>
+    <div className='container'>
       <h1 className='title'>Workday Website</h1>
       <Header onAdd={() => setShowAddJob(!showAddJob)}
       showAdd={showAddJob} />
       {showAddJob && <AddJob onAdd={addJob} />}
-      {jobs.length > 0 ? <Jobs jobs={jobs} onDelete={deleteJob} /> : 'no jobs to show'}
     </div>
+    <div className='gift'>
+    {jobs.length > 0 ? <Jobs jobs={jobs} onDelete={deleteJob} /> : 'No Jobs To Show'}
+    </div>
+    </>
   );
 }
 
